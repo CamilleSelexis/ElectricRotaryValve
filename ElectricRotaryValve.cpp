bf5f21a26ee21839723be5a98ff6ad487e434fb0
Camille <camille.aussems@selexis.com>
2022-08-31 10:40:19 +0200

    Updated, must be tested

--- ElectricRotaryValve.cpp
+++ ElectricRotaryValve.cpp
@@ -1,23 +1,43 @@
 #include "ElectricRotaryValve.h"
 
+/*base::base(int a, int b){
+	_a = a;
+	_b = b;
+}
+void base::hello_world(){
+	Serial.println("Hello world");
+	Serial.print("A is equal to ");Serial.println(_a);
+	Serial.print("B is equal to ");Serial.println(_b);
+}
 
-ERV(home_pin,port_pin,CS_pin,TGT_pin){
-	TMC4361A(CS_pin,TGT_pin);
+subclass::subclass(int a, int b, int c) : base(a,b){
+	_a = a;
+	_b = b;
+	_c = c;
+}
+
+void subclass::sub(){
+	Serial.print("A is equal to ");Serial.println(_a);
+	Serial.print("B is equal to ");Serial.println(_b);
+	Serial.print("C is equal to ");Serial.println(_c);
+}*/
+ERV::ERV(uint8_t home_pin, uint8_t port_pin, uint8_t CS_pin, uint8_t TGT_pin) : TMC4361A(CS_pin,TGT_pin){
+	//Controller = new TMC4361A(CS_pin,TGT_pin);
 	_home = home_pin;
 	_port = port_pin;
 }
 
-bool init_ERV(){
-	this.begin();
-	this.setCurrentScale(8);//Set current Scale for a NEMA 17 motor
-	this.setVMAX(45,2);//Set speed to 45 RPM
-	this.setAMAX(0x000FFF);//Set AMAX to a lower value
+bool ERV::init_ERV(){
+	TMC4361A::begin();
+	TMC4361A::setCurrentScale(8);//Set current Scale for a NEMA 17 motor
+	TMC4361A::setVMAX(45,2);//Set speed to 45 RPM
+	TMC4361A::setAMAX(0x000FFF);//Set AMAX to a lower value
 
-	this.setTargetRelative(51200);//1 full turn at 256 usteps
-	while(!this.isTargetReached()){
+	TMC4361A::setTargetRelative(51200);//1 full turn at 256 usteps
+	while(!TMC4361A::isTargetReached()){
 		if(!digitalRead(_home)){
-			this.setTargetRelative(0);
-			this.setCurrentPos(0);
+			TMC4361A::setTargetRelative(0);
+			TMC4361A::setCurrentPos(0);
 			return true;
 		}
 	}
@@ -27,28 +47,28 @@ bool init_ERV(){
 	return false;
 }
 
-bool openPort(int index){
+bool ERV::openPort(int index){
 	if(index<0 || index > 8)
 		return false;
 	long target = index*25*256;
-	this.setTarget(target); //Set absolute target
-	while(!this.isTargetReached());//wait for the motor to finish its move
+	TMC4361A::setTarget(target); //Set absolute target
+	while(!TMC4361A::isTargetReached());//wait for the motor to finish its move
 	if(digitalRead(_port))
 		return false; //Return false if after the move the port is not open
 	return true;
 }
 
-bool openHome(){
-	this.setTarget(0);
-	while(!this.isTargetReached());
+bool ERV::openHome(){
+	TMC4361A::setTarget(0);
+	while(!TMC4361A::isTargetReached());
 	if(digitalRead(_home))
 		return false;
 	return true;
 }
 
-int getOpenPort(){
+int ERV::getOpenPort(){
 	if(digitalRead(_port))
 		return -1;
-	float port = this.getCurrentPos()/(25*256);
+	float port = TMC4361A::getCurrentPos()/(25*256);
 	return  round(port);
 }
--- ElectricRotaryValve.h
+++ ElectricRotaryValve.h
@@ -1,19 +1,41 @@
+#ifndef _ELECTRICROTARYVALVE_H
+#define _ELECTRICROTARYVALVE_H
+
 #include "TMC4361A.h"
 #include "Arduino.h"
+
+/*class base {
+	public:
+		int _a;
+		int _b;
+		base(int a,int b);
+		void hello_world();
+};
+class subclass : public base
+{
+	public:
+		int _a;
+		int _b;
+		int _c;
+		subclass(int a, int b, int c);
+		void sub();
+};*/
 
 class ERV : public TMC4361A
 {
 
 	private:
-		int _home;
-		int _port;
-		
+		uint8_t _home;
+		uint8_t _port;
+
 	public:
-		ERV(home_pin,port_pin);
+		ERV(uint8_t home_pin,uint8_t port_pin, uint8_t cs_pin, uint8_t tgt_pin);
 		bool init_ERV();
 		bool openPort(int index);
 		bool openHome();
 		int getOpenPort();
 
 
-}
+};
+
+#endif
